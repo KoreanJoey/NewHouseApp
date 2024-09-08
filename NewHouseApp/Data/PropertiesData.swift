91faa5107401a8d4aaf72cb3aaf3e3efe70084e6
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-09-08 22:51:23 +1000

    Create PropertyDetailView

--- NewHouseApp/Data/PropertiesData.swift
+++ NewHouseApp/Data/PropertiesData.swift
@@ -11,8 +11,8 @@ import MapKit
 class PropertiesData {
     static let properties: [Property] = [
     Property(
-        name: "PARAMOUNT HURSTVILLE", coordinate: CLLocationCoordinate2D(latitude: -33.966486, longitude: 151.1001671),address: "2-10 Woniora Rd, Hurstville", description: "", imageNames: ["DJI_0261"]
+        name: "PARAMOUNT HURSTVILLE", coordinate: CLLocationCoordinate2D(latitude: -33.966486, longitude: 151.1001671),address: "2-10 Woniora Rd, Hurstville", description: "Come home to space, style, and a stunning outlook. Every Melrose Central residence is a collaboration between two industry leaders. The Deicorp team and TURNER studio have brought their design expertise to shape an outstanding residential experience, made for modern living.", imageNames: ["DJI_0261","1719559103887_9"]
     ),
-    Property(name: "MELOSE PARK", coordinate: CLLocationCoordinate2D(latitude: -33.8136438, longitude: 151.072535),address: "33 Hope Street, MelosePark", description: "", imageNames: ["1719559103887_9"])
+    Property(name: "MELOSE PARK", coordinate: CLLocationCoordinate2D(latitude: -33.8136438, longitude: 151.072535),address: "33 Hope Street, MelosePark", description: "Come home to space, style, and a stunning outlook. Every Melrose Central residence is a collaboration between two industry leaders.The Deicorp team and TURNER studio have brought their design expertise to shape an outstanding residential experience, made for modern living.", imageNames: ["1719559103887_9"])
     ]
 }
--- NewHouseApp/View/PropertyDetailView.swift
+++ NewHouseApp/View/PropertyDetailView.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct PropertyDetailView: View {
+    
+    let property: Property
+    @State var inquery: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TabView{
+                ForEach(property.imageNames, id: \.self) {
+                    image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            //.background(Color.blue)
+            .frame(height: 280)
+            .tabViewStyle(.page)
+            
+            HStack(alignment: .top){
+                VStack(alignment: .leading){
+                    Text(property.name)
+                        .font(.title3)
+                        .bold()
+                    Text(property.address)
+                    
+                }
+                //.background(Color.blue)
+                .padding(.horizontal)
+                Spacer()
+                HStack{
+                    
+                    Image(systemName: "star")
+                    Image(systemName: "square.and.arrow.up")
+                }
+                //.background(Color.blue)
+                    
+                
+            }
+            //.background(Color.red)
+            
+            Divider()
+                .background(Color.gray)
+                .frame(height: 5)
+            
+            VStack(alignment: .leading){
+                Text("Property Information")
+                    .font(.title3)
+                    .bold()
+                Text(property.description)
+            }
+            
+            Divider()
+                .background(Color.gray)
+                .frame(height: 5)
+            
+            VStack{
+                TextField("I want to ask about the \(property.name)", text: $inquery)
+                    .padding()
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.5), radius: 5, x: 0, y: 5)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 1))
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Send email to Agent")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                })
+                .background(Color.blue)
+                            .cornerRadius(2)
+                            .shadow(color: Color.gray.opacity(0.3), radius: 10, x: 0, y: 5)
+            }
+            .padding(.horizontal)
+            
+        }
+        .onAppear{
+            inquery = "I want to ask about the \(property.name)"
+        }
     }
 }
 
 #Preview {
-    PropertyDetailView()
+    PropertyDetailView(property: PropertiesData.properties.first!)
 }
